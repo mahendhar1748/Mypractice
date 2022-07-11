1a8a12afcc6b9273dc3152590020edb2a005841e
mahendhar1748@gmail.com <mahendhar1748@gmail.com>
2022-07-11 07:56:41 +0000

    2nd commit

---
+++ dira/dirb/file2.py
@@ -0,0 +1,5 @@
+x=int(input("entre the value of x:"))
+if(x%2 == 0):
+   print("the given number is even")
+else:
+   print("the given number is odd)
---
+++ dira/file1.py
@@ -0,0 +1,3 @@
+x=range(1,11)
+for p in x:
+    print(p)
